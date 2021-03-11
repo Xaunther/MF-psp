c5a5e672ec19a94d324c5eb7b4afb89d77474ef7
Alejandro Alfonso <aalfonso@cern.ch>
2021-03-11 22:29:48 +0100

    First commit. Made a hello world project!

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # MF-psp
-My first code for PSP
+
+My first code for PSP. Following the tutorial from [psp-dev](https://psp-dev.org/doku.php?id=tutorial:hello_world)
+
+## Summary
+
+Makes a Hello World project. Displays Hello World message in psp debug screen. Trying to exit crashes the console :D.
+
+## Compilation (Linux)
+
+Browse to the build folder and run `make` from there. You'll need to have installed the latest release of [pspdev](https://github.com/pspdev/pspdev). You need to add `<pspdevfolder>/bin` to your PATH variable.
+**I hope to learn many things!**
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <pspkernel.h>
+#include <pspdebug.h>
+
+PSP_MODULE_INFO("Tutorial", 0, 1, 0);
+
+int exit_callback(int arg1, int arg2, void *common)
+{
+    sceKernelExitGame();
+    return 0;
+}
+
+int CallbackThread(SceSize args, void *argp)
+{
+    int cbid = sceKernelCreateCallback("Exit Callback", exit_callback, NULL);
+    sceKernelRegisterExitCallback(cbid);
+    sceKernelSleepThreadCB();
+
+    return 0;
+}
+
+int SetupCallbacks(void)
+{
+    int thid = sceKernelCreateThread("update_thread", CallbackThread, 0x11, 0xFA0, 0, 0);
+    if (thid >= 0)
+    {
+        sceKernelStartThread(thid, 0, 0);
+    }
+    return thid;
+}
+
+int main()
+{ //In C++ `auto main() -> int` is also valid.
+    SetupCallbacks();
+    pspDebugScreenInit();
+    pspDebugScreenPrintf("Hello World!");
+    return 0;
+}
