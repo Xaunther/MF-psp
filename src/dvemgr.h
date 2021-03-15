3964aef675d884769512a1ac5d4dd207e0470913
Alejandro Alfonso <aalfonso@cern.ch>
2021-03-15 09:23:40 +0100

    Correctly linked with dvemgr
    Issue was 1) wasn't creating the object associated to the assembly code
    2) Not able to build dvemgr.prx with pure C++. Tried using mangled names in exports.exp did not work either
    3)As dvemgr.prx is built in C, linkage to the main app (c++) must be done with the extern "C" flags.
    
    Still many references to fix, we will see

--- src/dvemgr.h
+++ src/dvemgr.h
@@ -5,5 +5,15 @@
 ******************************************************************************/
 
 #pragma once
-int pspDveMgrCheckVideoOut();
-int pspDveMgrSetVideoOut(int u, int displaymode, int width, int height, int x, int y, int z);
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+	int pspDveMgrCheckVideoOut();
+	int pspDveMgrSetVideoOut(int u, int displaymode, int width, int height, int x, int y, int z);
+
+#ifdef __cplusplus
+}
+#endif
